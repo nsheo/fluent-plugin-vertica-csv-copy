b193385362e2a3826a5271278a475280d9adeeac
nsheo <nsheo@ntels.com>
2021-05-04 11:06:32 +0900

    edit for first test

--- lib/fluent/plugin/out_vertica_csv_copy.rb
+++ lib/fluent/plugin/out_vertica_csv_copy.rb
@@ -19,6 +19,129 @@ module Fluent
   module Plugin
     class VerticaCsvCopyOutput < Fluent::Plugin::Output
       Fluent::Plugin.register_output("vertica_csv_copy", self)
+	  
+	  def initialize
+        super
+        require 'vertica'
+        require 'tempfile'
+      end 
+	  
+	  config_param :host,           :string,  :default => '127.0.0.1', desc: "Database host"
+      config_param :port,           :integer, :default => 5433, desc: "Database port"
+      config_param :username,       :string,  :default => 'dbadmin', desc: "Database user"
+      config_param :password,       :string,  :default => nil, desc: "Database password"
+      config_param :database,       :string,  :default => nil, desc: "Database name"
+      config_param :schema,         :string,  :default => nil, desc: "Database schema"
+      config_param :table,          :string,  :default => nil, desc: "Database target table"
+	  config_param :column_names,   :string,  :default => nil, desc: "Column names for data load"
+	  config_param :key_names,      :string,  :default => nil, desc: "fleuntd target key, time can be override ${time}" 
+	  config_param :rejected_path,  :string,  :default => nil, desc: "File path for rejected data" 
+	  config_param :exception_path, :string,  :default => nil, desc: "File path for exception data" 
+	  config_param :key_names,      :string,  :default => nil, desc: "fleuntd target key, time can be override ${time}" 
+      config_param :ssl,            :bool,    :default => false, desc: "Database ssl connection info"
+	  
+	  def configure(conf)
+        compat_parameters_convert(conf, :buffer, :inject)
+        super
+        if @database.nil? || @table.nil? || @column_names.nil?
+          raise Fluent::ConfigError, "database and tablename and column_names is required."
+        end
+		
+		@key_names = @key_names.nil? ? @column_names.split(',') : @key_names.split(',')
+        unless @column_names.split(',').count == @key_names.count
+          raise Fluent::ConfigError, "It does not take the integrity of the key_names and column_names."
+        end
+      end
+	  
+      def start
+        super
+      end
+	  
+      def shutdown
+        super
+      end
+	  
+	  def format(tag, time, record)
+        record = inject_values_to_record(tag, time, record)
+        [tag, time, record].to_msgpack
+      end
+	  
+      def formatted_to_msgpack_binary
+        true
+      end
+	  
+      def multi_workers_ready?
+        true
+      end
+	  
+	  def expand_placeholders(metadata)
+        database = extract_placeholders(@database, metadata).gsub('.', '_')
+        table = extract_placeholders(@tablename, metadata).gsub('.', '_')
+        return database, table
+      end
+	  
+	  def write(chunk)
+        database, tablename = expand_placeholders(chunk.metadata)
+    		
+        data_count = 0
+        tmp = Tempfile.new("vertica-copy-temp")
+        chunk.msgpack_each do |tag, time, data|
+          tmp.write format_proc.call(tag, time, data).join("\t") + "\n"
+          data_count += 1
+        end
+		
+        tmp.close
+	    
+		vertica.copy(<<-SQL)
+          COPY #{table} (#{column_names})
+          FROM LOCAL tmp.path 
+		  DELIMITER E'\t'
+          RECORD TERMINATOR E'\n' 
+		  NULL AS '__NULL__'
+          ENFORCELENGTH
+	      ABORT ON ERROR
+		  NULL ''
+	      REJECTED DATA #{rejected_path}
+          EXCEPTIONS #{exception_path}
+		  DIRECT
+		  STREAM NAME 'Loading Data by fluentd'
+        SQL
+
+	    vertica.close
+        @vertica = nil
+		
+        log.info "Data loaded \"%s:%s\" table is %d" % ([@database, @tablename, data_count])
+      end
+
+	  
+	  private
+	  
+	  def format_proc
+        proc do |tag, time, record|
+          values = []
+          @key_names.each_with_index do |key, i|
+            if key == '${time}'
+              value = Time.at(time).strftime('%Y-%m-%d %H:%M:%S')
+            else
+              value = record[key]
+            end
+            values << value
+          end
+          values
+        end
+      end
+
+	  
+	  def vertica
+        @vertica ||= Vertica.connect({
+          :host     => @host,
+          :user     => @username,
+          :password => @password,
+          :ssl      => @ssl,
+          :port     => @port,
+          :database => @database
+        })
+      end
     end
   end
 end
